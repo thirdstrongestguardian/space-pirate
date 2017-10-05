204b2a96e17bf50d4775ac6c3bcddb338e853fad
William Hansen <william01@tutamail.com>
2017-10-04 18:26:15 -0700

    Getting the code to work in production.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var spacePirate = require('./lib/space-pirate');
 
+spacePirate.delete = require('./lib/delete');
+spacePirate.get = require('./lib/get');
+spacePirate.post = require('./lib/post-json');
+spacePirate.put = require('./lib/put');
+
 module.exports = spacePirate;
 
