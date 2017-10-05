e3598b3894822efa983d498723c6d59d3db6725b
William Hansen <william01@tutamail.com>
2017-10-04 18:27:45 -0700

    Fixed post method.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var spacePirate = require('./lib/space-pirate');
 
 spacePirate.delete = require('./lib/delete');
 spacePirate.get = require('./lib/get');
-spacePirate.post = require('./lib/post-json');
+spacePirate.post = require('./lib/post');
 spacePirate.put = require('./lib/put');
 
 module.exports = spacePirate;
